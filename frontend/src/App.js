1bd88fc9522e27d2e4d738dc064a38c1f2eb957e
Jean Robertou <jean.robertou@whereyoulove.com>
2020-10-05 09:13:20 +0200

    Header / Footer / Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React, { Fragment } from "react";
+import { Container } from "react-bootstrap";
+
+// COMPONENTS
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
 	return (
 		<Fragment>
-			<h1>Welcome to Proshop</h1>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<h1>Welcome to Proshop</h1>
+				</Container>
+			</main>
+			<Footer />
 		</Fragment>
 	);
 };
