8b000674234a1b6e1ea914eff03c5e846ccbd49b
Jean Robertou <jean.robertou@whereyoulove.com>
2020-10-06 10:03:36 +0200

    Create product reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const reducer = combineReducers({});
+// REDUCER
+import { productListReducer } from "./reducers/productReducers.js";
+
+const reducer = combineReducers({ productList: productListReducer });
 const initialState = {};
 
 const middleware = [ thunk ];
