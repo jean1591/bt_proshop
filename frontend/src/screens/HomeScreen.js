29bd91ebdb2aad2521e594e9f65ddef24d59d124
Jean Robertou <jean.robertou@whereyoulove.com>
2020-10-06 10:13:12 +0200

    Use reducer to fetch products

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,42 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
-import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
 
 // COMPONENTS
 import Product from "../components/Product";
 
+// REDUCER
+import { listProducts } from "../action/productActions.js";
+import { productListReducer } from "../reducers/productReducers";
+
 const HomeScreen = () => {
-	const [ products, setProducts ] = useState([]);
+	const dispatch = useDispatch();
+	const productList = useSelector((state) => state.productList);
+	const { loading, error, products } = productList;
 
-	useEffect(() => {
-		const fetchProducts = async () => {
-			const { data } = await axios.get("/api/products");
-
-			setProducts(data);
-		};
-		fetchProducts();
-	}, []);
+	useEffect(
+		() => {
+			dispatch(listProducts());
+		},
+		[ dispatch ]
+	);
 
 	return (
 		<Fragment>
 			<h1>Latest Products</h1>
-			<Row>
-				{products.map((product) => (
-					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>
+			{loading ? (
+				<h3>Loading...</h3>
+			) : error ? (
+				<h3>{error}</h3>
+			) : (
+				<Row>
+					{products.map((product) => (
+						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+							<Product product={product} />
+						</Col>
+					))}
+				</Row>
+			)}
 		</Fragment>
 	);
 };
