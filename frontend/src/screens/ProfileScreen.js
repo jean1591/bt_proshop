3f4b43e3a44419cd6210ea9ed8cbd5788b12428d
Jean Robertou <jean.robertou@whereyoulove.com>
2020-10-09 11:22:11 +0200

    Update User Profile

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -7,7 +7,7 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 
 // ACTIONS
-import { getUserDetails } from "../action/userActions.js";
+import { getUserDetails, updateUserProfile } from "../action/userActions.js";
 
 const ProfileScreen = ({ history }) => {
 	const [ name, setName ] = useState("");
@@ -23,6 +23,9 @@ const ProfileScreen = ({ history }) => {
 
 	const userLogin = useSelector((state) => state.userLogin);
 	const { userInfo } = userLogin;
+
+	const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+	const { success } = userUpdateProfile;
 
 	useEffect(
 		() => {
@@ -45,7 +48,7 @@ const ProfileScreen = ({ history }) => {
 		if (password !== confirmPassword) {
 			setMessage("Password do not match");
 		} else {
-			// dispatch update profile
+			dispatch(updateUserProfile({ id: user._id, name, email, password }));
 		}
 	};
 
@@ -55,6 +58,7 @@ const ProfileScreen = ({ history }) => {
 				<h2>User Profile</h2>
 				{message && <Message variant="danger">{message}</Message>}
 				{error && <Message variant="danger">{error}</Message>}
+				{success && <Message variant="success">Profile Updated</Message>}
 				{loading && <Loader />}
 				<Form onSubmit={submitHandler}>
 					<Form.Group controlId="name">
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,12 @@ import { composeWithDevTools } from "redux-devtools-extension";
 // REDUCER
 import { productListReducer, productDetailsReducer } from "./reducers/productReducers.js";
 import { cartReducer } from "./reducers/cartReducers";
-import { userLoginReducer, userRegisterReducer, userDetailsReducer } from "./reducers/userReducers";
+import {
+	userLoginReducer,
+	userRegisterReducer,
+	userDetailsReducer,
+	userUpdateProfileReducer
+} from "./reducers/userReducers";
 
 const reducer = combineReducers({
 	productList: productListReducer,
@@ -13,7 +18,8 @@ const reducer = combineReducers({
 	cart: cartReducer,
 	userLogin: userLoginReducer,
 	userRegister: userRegisterReducer,
-	userDetails: userDetailsReducer
+	userDetails: userDetailsReducer,
+	userUpdateProfile: userUpdateProfileReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
