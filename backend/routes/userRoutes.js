1b01417dda12edff49203ea1b3a2f9109bf85cfc
Jean Robertou <jean.robertou@whereyoulove.com>
2020-10-09 10:47:56 +0200

    Update User Backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,12 @@
 import express from "express";
 
 // CONTROLLERS
-import { authUser, registerUser, getUserProfile } from "../controllers/userController.js";
+import {
+	authUser,
+	registerUser,
+	getUserProfile,
+	updateUserProfile
+} from "../controllers/userController.js";
 
 // MIDDLEWARE
 import { protect } from "../middleware/authMiddleware.js";
@@ -10,6 +15,6 @@ const router = express.Router();
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
 
 export default router;
